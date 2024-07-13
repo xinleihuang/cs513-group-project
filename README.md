381d1640017c18c60950c9bcd9d6c5bbbdf84377
Oh <xinleihuang3@gmail.com>
2024-07-13 17:21:52 -0400

    add script to generate yesworkflow

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 ## NYPL Data
 raw and cleanded data can be found in [Google Drive Folder](https://drive.google.com/drive/folders/1FaB-ALfXPfsJnKVmrxm5h40c_2JqpLGO?usp=sharing). They can be accessed via Google account registered with university email.
+
+## Phase-2
+### generate yesworkflow files
+```bash
+cd phase-2
+./generate-yw.sh
+```
---
+++ phase-2/generate-yw.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source .venv/bin/activate
+
+mkdir -p yw/yw yw/gv yw/png
+
+for filepath in ./yw/history/*; do
+  if [ -f "$filepath" ]; then
+    filename=$(basename $filepath)
+    entity="${filename%.*}"
+    or2yw -i $filepath -o "yw/yw/${entity}.yw"
+    or2yw -i $filepath -o "yw/png/${entity}.png" -ot png
+    or2yw -i $filepath -o "yw/gv/${entity}.gv" -ot gv
+  fi
+done
